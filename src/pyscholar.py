f197b4981409579f2f99a1945a8c03f187cb952e
Ruy Fabila <ruy@ciencias.unam.mx>
2016-03-31 12:34:19 -0600

    Added the first module!

---
+++ src/pyscholar.py
@@ -0,0 +1,8 @@
+from scopus_key import MY_API_KEY
+import json
+import requests
+
+def find_author_scopus_id_by_name(name,aliases=[]):
+    """Searches for an author scopus id given its name. A list of aliases
+    may be added."""
+    pass
