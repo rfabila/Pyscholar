c5dc2a8ff93b47d26044535526277edd87956b59
Ruy Fabila <ruy@ciencias.unam.mx>
2016-04-05 13:26:51 -0500

    Added a few lines to Carlos'es code. put the variable for the url outside the function so that it is global on the module

--- src/pyscholar.py
+++ src/pyscholar.py
@@ -2,10 +2,12 @@ from scopus_key import MY_API_KEY
 import json
 import requests
 
+search_api_author_url = "http://api.elsevier.com/content/search/author?"
+
 def find_author_scopus_id_by_name(firstName="", lastName=""):
     """Searches for an author scopus id given its name."""
     
-    searchApi = "http://api.elsevier.com/content/search/author?"
+    
     headers = {"Accept":"application/json", "X-ELS-APIKey": MY_API_KEY}
     searchQuery = "query="
 
@@ -16,15 +18,22 @@ def find_author_scopus_id_by_name(firstName="", lastName=""):
             searchQuery += " AND "
         searchQuery += "AUTHLASTNAME(%s)" % (lastName)
     
+    #print searchQuery 
+    
     fields = "&field=identifier"
-    resp = requests.get(searchApi+searchQuery+fields, headers=headers)
+    resp = requests.get(search_api_author_url+searchQuery+fields, headers=headers)
     
     if resp.status_code != 200:
         print json.dumps(resp.json(), sort_keys=True, indent=4, separators=(',', ': '))
         return None
     
     data = resp.json()
+    #print "-----------JSON----------"
+    #print json.dumps(resp.json(), sort_keys=True, indent=4, separators=(',', ': '))
+    
+    #print "----------DATA----------"
     data = data['search-results']
+    #print data
     
     if data["opensearch:totalResults"] == '0':
         print "None"
