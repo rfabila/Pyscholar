51988867ce91ad2e6ceb9707fbc279f5924fa76b
Ruy Fabila <ruy@ciencias.unam.mx>
2016-04-05 22:31:49 -0500

    Added a todo list.
    -A way to manage the merging of ids
    
    -A wrapper to the search author api
    
    -A find_papers function

--- src/pyscholar.py
+++ src/pyscholar.py
@@ -4,6 +4,85 @@ import requests
 
 search_api_author_url = "http://api.elsevier.com/content/search/author?"
 
+#Un diccionario con las listas 
+Scopus_ids_merged_rep={}
+Scopus_ids_merged_lists={}
+
+#Voy a poner aqui lo que falta por hacer (TODO list) 
+
+#Funciones para manejar las ids, hago implementaciones ingenuas que
+#despues mejoraremos
+#Vean https://en.wikipedia.org/wiki/Disjoint-set_data_structure
+
+def _add_scopus_id(scopus_id):
+    """Adds a scopus id to the merged list. Returns False if the ID
+    is already in the merged list and false otherwise"""
+    
+    #Buscamos a ver si ya estaba en algunas lista, de ser
+    #asi no hacemos nada
+    for x in Scopus_ids_merged_lists:
+        if scopus_id in D[x]:
+            return False
+    
+    
+    #No tiene "padre" y por tanto es la raiz
+    Scopus_ids_merged_rep[scopus_id]=None
+    
+    #De momento el es el unico de la lista
+    Scopus_ids_merged_lists[scopus_id]=[scopus_id]
+    
+    return list
+
+
+def _get_root_id(scopus_id):
+    """Follows the path of parents until it finds the root"""
+    
+    #Si no esta en la lista de representantes pues no hay nada que hacer
+    if scopus_id not in Scopus_ids_merged_rep:
+        return scopus_id
+    
+    
+    root_scopus_id=scopus_id
+    parent_scopus_id=t_scopus_id
+    
+    #seguimos los apuntadores hasta llegar a la raiz
+    while Scopus_ids_merged_rep[root_scopus_id]!=None:
+        root_scopus_id=Scopus_ids_merged_rep[root_scopus_id]
+    
+    return root_scopus_id
+
+def _get_alias_list_id(scopus_id):
+    """returns the list of alias id"""
+    #En Scopus_ids_merged_lists[root_id] se van a guardar la lista
+    #de alias
+    
+def _union_alias_id(scopus_id_1,scopus_id_2):
+    #Junta a la lista de scopus_id_1 y scopus_id_2
+    pass
+        
+    
+def _get_alias_id(scopus_id):
+    pass
+    
+
+
+
+###FIN DE FUNCIONES de IDs
+
+def search_author():
+    """A wrapper to the Scopus API"""
+    #Pense que seria bueno un wrapper que provea todo lo del api
+    pass
+
+def get_papers(scopus_id):
+    """Returns the list of scopus id's of the papers of the given
+    author by scopus id"""
+    #Hay que considerar que pudieran tener aliases
+    pass
+    
+
+#FIN DE TODO LIST
+
 def find_author_scopus_id_by_name(firstName="", lastName=""):
     """Searches for an author scopus id given its name."""
     
