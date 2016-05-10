7bcb85344c3f7158ba2f1fb0489668d06c4205f5
Nemis <gerrymaths@gmail.com>
2016-05-10 01:04:57 -0500

    I updated the function get_coauthors_graph and get_citation_graph and I wrote the functions save_graph_pickle and load_graph_pickle

--- src/pyscholar.py
+++ src/pyscholar.py
@@ -121,6 +121,16 @@ def _get_alias_id(scopus_id):
 
 
 ###FIN DE FUNCIONES de IDs
+def load_graph_pickle(path=""):
+    """Read graph object in Python pickle format.
+    """
+    return nx.read_gpickle(path)
+
+def save_graph_pickle(G,path="",name_graph=""):
+    """
+    Save graph in Python pickle format.
+    """
+    nx.write_gpickle(G,path+name_graph+".gpickle")
 
 def find_affiliation_scopus_id_by_name(organization=""):
     """
@@ -200,7 +210,7 @@ def search_affiliation_by_id(list_scopus_id_affiliation):
         dict_affiliation_by_id[id_affiliation]=attributes
     return dict_affiliation_by_id
 
-def get_authors_by_id_affiliation(list_scopus_id_affiliation):
+def get_authors_by_id_affiliatio(list_scopus_id_affiliation):
     """Returns a dictionary where the key is the ID of the 
     paper and the value associated with the key is a set 
     of the ids of the papers cited by the main paper"""
@@ -396,13 +406,14 @@ def get_coauthors_graph(list_scopus_id_author,distance,min_year="",max_year="",d
                 continue
             else:
                 coauthors=get_coauthors(str(id_author),min_year,max_year,dict_knowledge_papers)
+                print coauthors
                 dict_knowledge_papers.update(coauthors[2])
                 for coauthor in coauthors[1]:
                     edge_list.append((id_author,str(coauthor)))
                     attribute_edge.append((id_author,str(coauthor),coauthors[2][coauthor]))
                     new_search.add(str(coauthor))
         if (iteration==1):
-            #print list_scopus_id_author
+            print list_scopus_id_author
             dict_last_authors=dict()
             list_scopus_id_author_found=set()
             for id_author in list_scopus_id_author:
@@ -415,12 +426,10 @@ def get_coauthors_graph(list_scopus_id_author,distance,min_year="",max_year="",d
                     resource_not_found.append(id_author)
                     continue
             check_edge=it.combinations(list_scopus_id_author_found,2)
-            #print dict_last_authors
             for edge in check_edge:
-                #print type(edge[0]),edge[1]
-                intersection_papers=dict_last_authors[edge[0]].intersection(dict_last_authors[edge[1]])
+                #print edge[0],edge[1]
+                intersection_papers=get_common_papers(edge[0],edge[1])
                 if len(intersection_papers)>0:
-                    #print number_paper
                     edge_list.append((edge[0],edge[1]))
                     attribute_edge.append((edge[0],edge[1],intersection_papers))
         list_scopus_id_author=new_search.copy()
@@ -467,8 +476,8 @@ def get_citation_graph(list_scopus_id_paper,distance,directory="",name=""):
     dist_count=0
     D=[]
     while(iteration!=0):
-        print list_scopus_id_paper
-        print len(list_scopus_id_paper)
+        #print list_scopus_id_paper
+        #print len(list_scopus_id_paper)
         new_search=set()
         for paper in list_scopus_id_paper:
             if paper not in nodes:
