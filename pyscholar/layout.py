d766fbb4164b1d6066cb27e47620924e23fd8b57
luisminerog <luisminerog@gmail.com>
2017-10-23 22:32:09 -0500

    refactor: Add whitespaces in assignments and comparisons.

--- pyscholar/layout.py
+++ pyscholar/layout.py
@@ -4,20 +4,21 @@ import xml.etree.ElementTree as ET
 def get_node_dict_layout_from_graphml_file(filename):
     """Reads the given graphml file an extracts the nodes attributes. The idea is that the graphml was
     probably modified in gephi."""
-    D={}
+    D = {}
     tree = ET.parse(filename)
-    root=tree.getroot()
+    root = tree.getroot()
     for v in root.iter("{http://graphml.graphdrawing.org/xmlns}node"):
-        name=v.attrib['id']
+        name = v.attrib['id']
         #print name
-        d={}
+        d = {}
         for ch in v:
-            d[ch.attrib['key']]=ch.text
-        D[name]=d
+            d[ch.attrib['key']] = ch.text
+        D[name] = d
     return D
 
-def apply_layout(H,D):
+
+def apply_layout(H, D):
     for v in H.nodes():
         if v in D:
             for key in D[v]:
-                H.node[v][key]=D[v][key]
+                H.node[v][key] = D[v][key]
